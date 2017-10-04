e3b378735fcc9343f9e52bdbe496efabf657ceb3
akshat <akshatbhargavadeepa@gmail.com>
2017-10-05 02:05:07 +0530

    Basic updation for navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,36 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+// Firebase - Angularfire2 dependencies
+import { FIREBASE_CONFIG } from './firebase.config';
+import { AngularFireModule } from "angularfire2";
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule }  from 'angularfire2/database';
+
+// Providers
+import { LoginAuthenticatorProvider } from "./../providers/login-authenticator/login-authenticator";
+
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    LoginAuthenticatorProvider,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
