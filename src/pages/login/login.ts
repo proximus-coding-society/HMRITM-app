09413d82ba8cbeefb5fa2d73cc0dc179edc9414e
akshat <akshatbhargavadeepa@gmail.com>
2017-10-05 03:13:22 +0530

    Added file

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { StudentSignupModel } from './../../models/user.models';
 
 @IonicPage()
 @Component({
@@ -15,11 +10,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
+  studentDetails: StudentSignupModel;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  login() {
+
+  }
+
+  forgotPassword() {
+
+  }
+
+  register() {
+
   }
 
 }
