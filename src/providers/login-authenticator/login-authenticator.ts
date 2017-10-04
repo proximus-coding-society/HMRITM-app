f16b6f5730ac857ab8487b86d89656dda9d8643c
akshat <akshatbhargavadeepa@gmail.com>
2017-10-05 02:05:35 +0530

    Made login service via firebase

--- src/providers/login-authenticator/login-authenticator.ts
+++ src/providers/login-authenticator/login-authenticator.ts
@@ -1,18 +1,53 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import firebase from "firebase/app";
 
-/*
-  Generated class for the LoginAuthenticatorProvider provider.
+import { StudentSignupModel } from '../../models/user.models';
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class LoginAuthenticatorProvider {
 
-  constructor(public http: Http) {
+  constructor(public afAuthService: AngularFireAuth,
+              public afDbService: AngularFireDatabase) {
     console.log('Hello LoginAuthenticatorProvider Provider');
   }
 
+  signup(details: StudentSignupModel) {
+    return this.afAuthService.auth.createUserWithEmailAndPassword(details.email, details.password)
+      .then((user) => {
+
+        this.afDbService.object(`/users/students/${this.afAuthService.auth.currentUser.uid}`).set({
+          email: details.email,
+          name: details.name,
+          class: details.class,
+          contact_no: details.contact_no,
+          roll_no: details.roll_no,
+          enrollment_no: details.enrollment_no,
+        });
+
+        console.log('User Registered!');
+
+      });
+  }
+
+  login(newEmail: string, newPassword: string){
+    return this.afAuthService.auth.signInWithEmailAndPassword(newEmail, newPassword);
+  }
+
+  resetPassword(email: string) {
+    return this.afAuthService.auth.sendPasswordResetEmail(email);
+  }
+
+  logout() {
+    return this.afAuthService.auth.signOut();
+  }
+
+  getCurrentUser(): any {
+    return this.afAuthService.auth.currentUser;
+  }
+
+  getAuthState(): any {
+    return this.afAuthService.authState;
+  }
 }
