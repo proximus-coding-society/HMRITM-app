4beff03ec0f4fb4b0f2c9e2bff5548501b36d739
akshat <akshatbhargavadeepa@gmail.com>
2017-10-05 02:04:41 +0530

    firebase config setup

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyC1gnyOzSqidfehAPYvSoG9coYCXlmglUU",
+    authDomain: "hmritm-app-cb87a.firebaseapp.com",
+    databaseURL: "https://hmritm-app-cb87a.firebaseio.com",
+    projectId: "hmritm-app-cb87a",
+    storageBucket: "hmritm-app-cb87a.appspot.com",
+    messagingSenderId: "772298742101"
+};
