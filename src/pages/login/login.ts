fe8facebd1ae395d158bc2f93e0b8b65b7853a44
akshat <akshatbhargavadeepa@gmail.com>
2017-10-05 21:01:20 +0530

    Started mock login and navigating forward

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 
 import { StudentSignupModel } from './../../models/user.models';
+import { LoginAuthenticatorProvider } from './../../providers/login-authenticator/login-authenticator';
 
 @IonicPage()
 @Component({
@@ -10,13 +11,26 @@ import { StudentSignupModel } from './../../models/user.models';
 })
 export class LoginPage {
 
-  studentDetails: StudentSignupModel;
+  studentDetails = {} as StudentSignupModel;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public loadingCtrl: LoadingController,
+              public loginAuth: LoginAuthenticatorProvider) {
   }
 
   login() {
-
+    let loader = this.loadingCtrl.create({
+      content: 'Authenticating...',
+      dismissOnPageChange: true
+    });
+    loader.present();
+    this.loginAuth.login(this.studentDetails.email, this.studentDetails.password).then((isLoggedIn) => {
+      if (isLoggedIn) {
+        loader.dismiss();
+        this.navCtrl.setRoot('MenuPage');
+      }
+    });
   }
 
   forgotPassword() {
