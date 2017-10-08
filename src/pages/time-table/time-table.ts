1617bea6fb2f6cbd62a41e91ccaf1553378649c4
akshat <akshatbhargavadeepa@gmail.com>
2017-10-08 09:23:09 +0530

    Completed time table page

--- src/pages/time-table/time-table.ts
+++ src/pages/time-table/time-table.ts
@@ -1,12 +1,7 @@
+import { DateTimeProvider } from './../../providers/date-time/date-time';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the TimeTablePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @IonicPage()
 @Component({
@@ -15,11 +10,45 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TimeTablePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  daysList: {
+    title: string;
+    teacherList?: any;
+  }[];
+  
+  time_table: any;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TimeTablePage');
+  constructor(public navCtrl: NavController, public navParams: NavParams, public dateTime: DateTimeProvider, public afDb: AngularFireDatabase) {
+    
+    let now = this.dateTime.getDayHoursMinNow();
+    console.log(now);
+    
+    this.afDb.object(`time_table/${this.navParams.data.user.data.class}`).valueChanges().subscribe(time_table => {
+      this.time_table = time_table;
+      this.daysList = [
+        {
+          title: 'monday'
+        },
+        {
+          title: 'tuesday',
+        },
+        {
+          title: 'wednesday',
+        },
+        {
+          title: 'thursday',
+        },
+        {
+          title: 'friday',
+        }
+      ];
+      for (let i = 0; i < this.daysList.length; i++) {
+        let teacherList = [];
+        for (let j = 0; j < 8; j++) {
+          teacherList.push(this.navParams.data.teachers[j]);          
+        }
+        this.daysList[i].teacherList = teacherList;
+      }
+    });
   }
 
 }
