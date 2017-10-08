e751ca1e79ae2485195b317fbfb9a0ab3f783f90
akshat <akshatbhargavadeepa@gmail.com>
2017-10-08 09:18:47 +0530

    Data will now flow page to page instead fetching online again and again.

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -28,7 +28,26 @@ export class LoginPage {
     this.loginAuth.login(this.studentDetails.email, this.studentDetails.password).then((isLoggedIn) => {
       if (isLoggedIn) {
         loader.dismiss();
-        this.navCtrl.setRoot('MenuPage');
+        let dataLoader = this.loadingCtrl.create({
+          content: 'Gathering your data',
+          dismissOnPageChange: true
+        });
+        dataLoader.present();
+        this.loginAuth.afDbService.object(`users/students/${this.loginAuth.getCurrentUser().uid}`).valueChanges().subscribe((data) => {
+          this.loginAuth.afDbService.object(`subjects`).valueChanges().subscribe((subjects) => {
+            this.loginAuth.afDbService.object(`teachers`).valueChanges().subscribe((teachers) => {
+              dataLoader.dismiss();
+              this.navCtrl.setRoot('MenuPage', {
+                user: {
+                  data: data,
+                  uid: this.loginAuth.getCurrentUser().uid
+                },
+                subjects: subjects,
+                teachers: teachers
+              });
+            });
+          });
+        });
       }
     });
   }
