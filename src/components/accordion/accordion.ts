a0cefa1b93b7f90d17940cde27a5b3fbfdaa1746
akshat <akshatbhargavadeepa@gmail.com>
2017-10-08 21:25:13 +0530

    Updated accordion component

--- src/components/accordion/accordion.ts
+++ src/components/accordion/accordion.ts
@@ -6,32 +6,47 @@ import { Component, ViewChild, OnInit, Renderer, Input } from '@angular/core';
 })
 export class AccordionComponent implements OnInit
 {
-  accordionExpanded = false;
-  @Input('title') title_in;
-  @Input('color') color_in;
-  // @Input('itemList') list_in: Array<any>;
+  @Input('expanded') accordionExpanded_in: any;
+  @Input('title') title_in: string;
+  @Input('color') color_in: string;
   @ViewChild("cc") cardContent: any;
+  iconName: string;
 
   constructor(public renderer : Renderer) {
     
   }
+
   ngOnInit()
   {
+    // console.log(typeof(this.accordionExpanded_in))
+    if (this.accordionExpanded_in == null) {
+      this.accordionExpanded_in = false;
+      this.iconName = "arrow-forward";
+      console.log('setting accordion to false explicitly');
+    }
     this.renderer.setElementStyle(this.cardContent.nativeElement, "webkitTransition", "max-height 100ms, padding 100ms");
+    this.initAccordion();
   }
-  toggleAccordion()
-  {
-    if(this.accordionExpanded)
+
+  initAccordion() {
+    if(this.accordionExpanded_in)
     {
-      this.renderer.setElementStyle(this.cardContent.nativeElement, "max-height","0px");
-      this.renderer.setElementStyle(this.cardContent.nativeElement, "padding","0px 16px");
+      this.renderer.setElementStyle(this.cardContent.nativeElement, "max-height","6000px");
+      this.renderer.setElementStyle(this.cardContent.nativeElement, "padding","0px 0px");
+      this.iconName = "arrow-forward";
     }
     else
     {
-      this.renderer.setElementStyle(this.cardContent.nativeElement, "max-height","2500px");
-      this.renderer.setElementStyle(this.cardContent.nativeElement, "padding","0px 0px");
+      this.renderer.setElementStyle(this.cardContent.nativeElement, "max-height","0px");
+      this.renderer.setElementStyle(this.cardContent.nativeElement, "padding","0px 16px");
+      this.iconName = "arrow-down";
     }
-    this.accordionExpanded = !this.accordionExpanded;
+  }
+
+  toggleAccordion() {
+    this.accordionExpanded_in = !this.accordionExpanded_in;
+    this.iconName = this.accordionExpanded_in ? "arrow-down" : "arrow-forward";
+    this.initAccordion();
   }
 
 }
