e6bc286fbc02b5d5ed5e96c69149b41c52db7e9a
akshat <akshatbhargavadeepa@gmail.com>
2017-10-08 09:21:57 +0530

    Added more menu options

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams, Nav } from 'ionic-angular';
+import { IonicPage, Nav, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -17,14 +17,49 @@ export class MenuPage {
     icon: string;
   }>;
   
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navParams: NavParams) {
     this.pages = [
       {
         title: 'Home',
         handler: () => {
-          this.nav.setRoot('TabsPage');
+          this.nav.setRoot('TabsPage', this.navParams.data);
         },
         icon: 'home'
+      },
+      {
+        title: 'Time Table',
+        handler: () => {
+          this.nav.setRoot('TimeTablePage', this.navParams.data);
+        },
+        icon: 'calendar'
+      },
+      {
+        title: 'Planner',
+        handler: () => {
+          this.nav.setRoot('PlannerPage', this.navParams.data);
+        },
+        icon: 'bookmarks'
+      },
+      {
+        title: 'Attendance',
+        handler: () => {
+          this.nav.setRoot('AttendancePage', this.navParams.data);
+        },
+        icon: 'body'
+      },
+      {
+        title: 'Assignment Manager',
+        handler: () => {
+          this.nav.setRoot('AssignmentPage', this.navParams.data);
+        },
+        icon: 'checkbox-outline'
+      },
+      {
+        title: 'Map to College',
+        handler: () => {
+          this.nav.setRoot('CollegeMapPage', this.navParams.data);
+        },
+        icon: 'locate'
       }
     ];
   }
